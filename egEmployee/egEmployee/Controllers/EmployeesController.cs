a258631668e80f3ee3ab8504910278a318ede217
JooYoo <jooyoo@outlook.com>
2021-11-19 10:37:06 +0100

    Add files via upload

---
+++ egEmployee/egEmployee/Controllers/EmployeesController.cs
@@ -0,0 +1,31 @@
+﻿using egEmployee.Data;
+using egEmployee.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace egEmployee.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+        // dependency injection: include EmployeeSalaryRepository
+        private readonly EmployeeSalaryRepository _repository;
+        public EmployeesController(EmployeeSalaryRepository repository)
+        {
+            this._repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        // async task: connect with EmployeeSalaryRepo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmployeeSalary>>> Get()
+        {
+            return await _repository.GetSalaries();
+        }
+
+    }
+}
---
+++ egEmployee/egEmployee/Models/EmployeeSalary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace egEmployee.Models
+{
+    public class EmployeeSalary
+    {
+        public int EmployeeID { get; set; }
+        public string JobTitle { get; set; }
+        public int Salary { get; set; }
+    }
+}

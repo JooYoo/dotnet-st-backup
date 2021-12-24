798cf5b31b5b0e353a96f4ccbeff70ed14fea3cf
jooyoo <jooyoo@outlook.com>
2021-12-24 09:15:15 +0100

    feat: url based versioning

--- egRelationalDT/egRelationalDT/Controllers/v1/TestController.cs
+++ egRelationalDT/egRelationalDT/Controllers/v1/TestController.cs
@@ -1,14 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace egRelationalDT.Controllers.v1
 {
     [ApiVersion("1.0")]
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class TestController : ControllerBase
     {

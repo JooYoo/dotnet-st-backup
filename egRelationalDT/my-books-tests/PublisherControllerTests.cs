13a028a83049167065aa0b1c4ab15694b602072d
jooyoo <jooyoo@outlook.com>
2022-01-17 08:00:07 +0100

    test: HTTPGET_GetAllPublishers_WithSortBy_Search_PageNr_Test

--- egRelationalDT/my-books-tests/PublisherControllerTests.cs
+++ egRelationalDT/my-books-tests/PublisherControllerTests.cs
@@ -2,12 +2,12 @@
 using egRelationalDT.Data;
 using egRelationalDT.Data.Models;
 using egRelationalDT.Data.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging.Abstractions;
 using NUnit.Framework;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace my_books_tests
 {
@@ -37,6 +37,24 @@ namespace my_books_tests
             // init PublisherService
             publisherService = new PublisherService(context);
             publishersController = new PublishersController(publisherService, new NullLogger<PublishersController>());
+        }
+
+        [Test, Order(1)]
+        public void HTTPGET_GetAllPublishers_WithSortBy_Search_PageNr_Test()
+        {
+            IActionResult actionResult = publishersController.GetAllPublishers("name_desc", "Publisher", 1);
+            Assert.That(actionResult, Is.TypeOf<OkObjectResult>());
+            var actionResultData = (actionResult as OkObjectResult).Value as List<Publisher>;
+            Assert.That(actionResultData.First().Name, Is.EqualTo("Publisher 6"));
+            Assert.That(actionResultData.First().Id, Is.EqualTo(6));
+            Assert.That(actionResultData.Count, Is.EqualTo(5));
+
+            IActionResult actionResultSecondPage = publishersController.GetAllPublishers("name_desc", "Publisher", 2);
+            Assert.That(actionResultSecondPage, Is.TypeOf<OkObjectResult>());
+            var actionResultDataSecondPage = (actionResultSecondPage as OkObjectResult).Value as List<Publisher>;
+            Assert.That(actionResultDataSecondPage.First().Name, Is.EqualTo("Publisher 1"));
+            Assert.That(actionResultDataSecondPage.First().Id, Is.EqualTo(1));
+            Assert.That(actionResultDataSecondPage.Count, Is.EqualTo(1));
         }
 
 
@@ -74,20 +92,12 @@ namespace my_books_tests
                     new Publisher() {
                         Id = 6,
                         Name = "Publisher 6"
-                    },
-                     new Publisher() {
-                        Id = 7,
-                        Name = "Publisher 7"
-                    },
+                    }
             };
             context.Publishers.AddRange(publishers);
 
-       
-
-
             context.SaveChanges();
         }
-
 
     }
 }
